5af40803c45a1b2a2f96747b6117acf5990c628e
thoyoin <lukharchik@gmail.com>
2025-06-11 12:53:21 +0300

    deployed

--- server/database.js
+++ server/database.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config();
+const env = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${env}` });
+
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: process.env.DB_HOST || 5432,
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         logging: false,
     }
