bb4f9a223c9ddbc755587ff774ec7f29acaac98c
thoyoin <lukharchik@gmail.com>
2025-06-25 10:32:05 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
+app.use(cors(/* {
     origin: ['http://localhost:3000', 'https://course-project-bjk6.onrender.com'],
     credentials: true,
-}));
+} */));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
