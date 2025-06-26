893b3c751d1fb354018e2f70ccc4ceaf743c8423
thoyoin <lukharchik@gmail.com>
2025-06-26 16:09:12 +0300

    form page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import CreateTemplate from './components/CreateTemplate';
 import Login from './components/Login';
 import MainPage from './components/MainPage';
 import Registration from './components/Registration';
+import FormPage from './components/FormPage';
 import './index.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Routes, Route, Navigate } from 'react-router-dom';
@@ -16,6 +17,7 @@ function App() {
         <Route path='/Registration' element={<Registration />} />
         <Route path='/MainPage' element={<MainPage />} />
         <Route path='/CreateTemplate/:templateId' element={<CreateTemplate />} />
+        <Route path='/FormPage/:templateId' element={<FormPage />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/FormPage.js
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import ChangeLang from './ChangeLang';
+import LogOutBtn from './LogOutBtn';
+import { useTranslation } from 'react-i18next';
+import ModalPublishBtn from './ModalPublishBtn';
+
+const FormPage = () => {
+    const { templateId, formikValues } = useParams();
+    const [template, setTemplate] = useState(null);
+    const [answers, setAnswers] = useState({});
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+
+
+    useEffect(() => {
+        const fetchTemplate = async () => {
+        try {
+            const res = await fetch(`https://course-project-back-tv8f.onrender.com/api/templates/${templateId}`);
+            const data = await res.json();
+            setTemplate(data);
+        } catch (err) {
+            console.error('Failed to load template', err);
+        }
+        };
+
+        fetchTemplate();
+    }, [templateId]);
+
+    const handleChange = (index, value) => {
+        setAnswers(prev => ({ ...prev, [index]: value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+
+        try {
+        const res = await fetch(`https://course-project-back-tv8f.onrender.com/api/forms/${templateId}/submit`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ answers }),
+        });
+
+        if (res.ok) {
+            alert('Form submitted!');
+            navigate('/me');
+        } else {
+            alert('Failed to submit form.');
+        }
+        } catch (err) {
+        console.error('Submit error:', err);
+        }
+    };
+
+    if (!template) return <p>Loading template...</p>;
+
+    console.log('Loaded template:', template);
+    console.log('Template questions:', template.questions);
+
+    return (
+        <div>
+            <div style={{height:'68px', zIndex:'100'}} className='container-fluid d-flex flex-row justify-content-between align-items-center position-fixed bg-body-tertiary top-0 border-bottom'>
+                <div className='ms-4 d-flex flex-row align-items-center gap-4'>
+                    <a href='/MainPage' className='text-success link-ease-in-out' onClick={() => navigate('/MainPage')}><i alt='home' className="bi bi-file-earmark-text-fill fs-2"></i></a>
+                    <h4 className='fw-bold m-0'>{template.templateName}</h4>
+                </div>
+                <div className='d-flex flex-row align-items-center'>
+                    <button 
+                        data-bs-toggle="modal"
+                        data-bs-target="#deleteModal"
+                        style={{height:'35px'}} 
+                        className='btn btn-danger mx-5 px-3 py-2 d-flex flex-row align-items-center'
+                    >
+                        <i className="bi bi-trash me-2"></i>
+                        {t('delete')}
+                    </button>
+                    <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">{t('sure?')}</h1>
+                            </div>
+                            <div className="modal-footer ">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">{t('cancel')}</button>
+                                <button type="button" className="btn btn-danger" data-bs-dismiss="modal"
+                                onClick={() => {
+                                  /*   handleDeleteTemplate(); */
+                                }}>{t('delete')}</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ModalPublishBtn templateId={templateId} /* formikValues={formik.values} *//>
+                    <ChangeLang/>
+                    <LogOutBtn/>
+                </div>
+            </div>
+            <div className='d-flex flex-column justify-content-start align-items-center'>
+                    <div style={{maxWidth:'700px', minHeight:'170px', marginTop:'100px'}} className="w-100">
+                            <div style={{minHeight:'170px'}} className='bg-body-tertiary mx-3 mb-4 text-start rounded-4 border border-success border-1 rounded-4'>
+                                <h2
+                                    className="mt-3 ps-4 fs-1 fw-bold form-control border-0 rounded-0 bg-body-tertiary" 
+                                    >{template.templateName}</h2>
+                                <p 
+                                    className='form-control ps-4 text-muted border-0 border-bottom border-success rounded-0 bg-body-tertiary'
+                                    >{template.description}</p>
+                            </div>
+                    </div>
+                <form style={{maxWidth:'700px'}} className='w-100 text-center' onSubmit={handleSubmit}>
+                    {(template.questions || template.newQuestion)?.map((q, idx) => (
+                        <div key={idx} className="mb-4">
+                        <div className='d-flex flex-row justify-content-center align-items-center'>
+                                    <div style={{minHeight:'150px', marginTop:'15px'}} className='bg-body-tertiary w-100 mx-3 text-start border rounded-4 d-flex flex-column justify-content-start'>
+                                        <label className="form-label p-4 fs-5">{q.text}</label>
+                                        {q.questionType === 'short text' && (
+                                        <input
+                                            type="text" 
+                                            placeholder={t('myAnswer')}
+                                            style={{outline:'none', boxShadow:'none'}} 
+                                            className="form-control w-50 ms-4 p-0 pb-1 border-0 border-bottom border-success rounded-0 bg-body-tertiary" 
+                                            onChange={e => handleChange(idx, e.target.value)} />
+                                        )}
+                                        {q.questionType === 'long text' && (
+                                        <textarea className="form-control" onChange={e => handleChange(idx, e.target.value)} />
+                                        )}
+                                        {q.questionType === 'checkbox' && q.checkboxOptions?.map((opt, i) => (
+                                        <div key={i} className="form-check">
+                                            <input
+                                            type="checkbox"
+                                            className="form-check-input ms-1"
+                                            style={{outline:'none', boxShadow:'none'}}
+                                            id={`check-${idx}-${i}`}
+                                            onChange={e => {
+                                                const prev = answers[idx] || [];
+                                                const updated = e.target.checked
+                                                ? [...prev, opt]
+                                                : prev.filter(item => item !== opt);
+                                                handleChange(idx, updated);
+                                            }}
+                                            />
+                                            <label className="form-check-label ms-3" htmlFor={`check-${idx}-${i}`}>{opt}</label>
+                                        </div>
+                                        ))}
+                                    </div>
+                                </div>
+                    </div>
+                    ))}
+                    <button type="submit" className="btn btn-success mt-2 mb-5 mx-3">Submit form</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+
+export default FormPage;
