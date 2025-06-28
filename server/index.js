e8562b69ffbae19c0ed7624f99dcebf598c574aa
thoyoin <lukharchik@gmail.com>
2025-06-28 21:38:24 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/templates', templateRoutes);
-app.use('/app/forms', formRoutes);
+app.use('/api/forms', formRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is working!');
